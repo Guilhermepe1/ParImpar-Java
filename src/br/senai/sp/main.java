291e45c8af0af785f17a2db7bc730ec735cc84cc
VITOR SANTANA DE JESUS <vitor.alunos@cfp127aluno.local>
2023-08-15 11:45:44 -0300

    ParImpar

--- src/br/senai/sp/main.java
+++ src/br/senai/sp/main.java
@@ -7,12 +7,12 @@ public class main {
     public static void main(String[] args) {
 
         /** Declara variaveis */
-        int number = 0;
+        int number = 3;
 
         /** Instancia teclado */
         Scanner teclado = new Scanner(System.in);
 
-        while (number%2==0) {
+        while (number%2!=0) {
             /** Coleta dados */
             System.out.println("________________________________");
             System.out.print("Digite um número: ");
